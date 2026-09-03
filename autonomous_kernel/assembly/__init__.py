872d373125d1a17d8657827c7600781e5e9080b3
B Geo 22 <161180938+Geo222222@users.noreply.github.com>
2026-09-03 09:06:07 -0500

    z9: export governed context profile registry

--- autonomous_kernel/assembly/__init__.py
+++ autonomous_kernel/assembly/__init__.py
@@ -6,6 +6,7 @@ Z8 assembly, but neither layer allocates capital, authorizes risk, or executes.
 
 from .adaptive import AdaptiveAssemblyError, assemble_prediction
 from .contracts import ASSEMBLY_SCHEMA_VERSION, INTERVAL_POLICY_ID, WEIGHT_POLICY_ID, AssemblyContractError, AssemblyReceipt
+from .context_profile_registry import ModelContextProfileRegistry, ModelContextProfileRegistryError, profile_from_mapping, validate_model_context_profile_registry
 from .contextual import CONTEXTUAL_ASSEMBLY_SCHEMA_VERSION, CONTEXT_MULTIPLIER_MAX, CONTEXT_MULTIPLIER_MIN, CONTEXT_WEIGHT_POLICY_ID, ContextualAssemblyError, ContextualAssemblyReceipt, ModelContextProfile, contextualize_prediction
 from .contextual_journal import ContextualAssemblyJournal, ContextualAssemblyJournalError, validate_contextual_assembly_journal
 from .contextual_lineage import validate_contextual_assembly_lineage, validate_contextual_receipt_lineage
@@ -16,5 +17,5 @@ from .service import CertifiedAssemblyError, assemble_and_record
 
 __all__ = [
     "ASSEMBLY_SCHEMA_VERSION", "INTERVAL_POLICY_ID", "WEIGHT_POLICY_ID", "AdaptiveAssemblyError", "AssemblyContractError", "AssemblyJournal", "AssemblyJournalError", "AssemblyReceipt", "CertifiedAssemblyError", "assemble_and_record", "assemble_prediction", "validate_assembly_journal", "validate_assembly_lineage", "validate_assembly_receipt_lineage",
-    "CONTEXTUAL_ASSEMBLY_SCHEMA_VERSION", "CONTEXT_MULTIPLIER_MAX", "CONTEXT_MULTIPLIER_MIN", "CONTEXT_WEIGHT_POLICY_ID", "ContextualAssemblyError", "ContextualAssemblyJournal", "ContextualAssemblyJournalError", "ContextualAssemblyReceipt", "ModelContextProfile", "CertifiedContextualAssemblyError", "contextual_assemble_and_record", "contextualize_prediction", "validate_contextual_assembly_journal", "validate_contextual_assembly_lineage", "validate_contextual_receipt_lineage",
+    "CONTEXTUAL_ASSEMBLY_SCHEMA_VERSION", "CONTEXT_MULTIPLIER_MAX", "CONTEXT_MULTIPLIER_MIN", "CONTEXT_WEIGHT_POLICY_ID", "ContextualAssemblyError", "ContextualAssemblyJournal", "ContextualAssemblyJournalError", "ContextualAssemblyReceipt", "ModelContextProfile", "ModelContextProfileRegistry", "ModelContextProfileRegistryError", "CertifiedContextualAssemblyError", "contextual_assemble_and_record", "contextualize_prediction", "profile_from_mapping", "validate_contextual_assembly_journal", "validate_contextual_assembly_lineage", "validate_contextual_receipt_lineage", "validate_model_context_profile_registry",
 ]
