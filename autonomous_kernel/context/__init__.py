5d401920190ea3666024ec328644059e1a02d44b
B Geo 22 <161180938+Geo222222@users.noreply.github.com>
2026-09-03 09:05:49 -0500

    z9: export operational materializer

--- autonomous_kernel/context/__init__.py
+++ autonomous_kernel/context/__init__.py
@@ -2,6 +2,7 @@
 
 from .builder import BUILDER_VERSION, MarketContextBuildError, build_market_context
 from .contracts import CONTEXT_SCHEMA_VERSION, CONTEXT_STATUSES, CONTEXT_TYPES, MarketContextContractError, MarketContextFrame
+from .materialize import MATERIALIZER_POLICY_ID, MATERIALIZER_SELECTION_RULE, MarketContextMaterializationError, materialize_market_context, select_durable_representation_frames, validate_market_context_materializations, verify_materialized_context
 from .status import market_context_status
 from .store import MarketContextStore, validate_market_context_store
 
@@ -10,11 +11,18 @@ __all__ = [
     "CONTEXT_SCHEMA_VERSION",
     "CONTEXT_STATUSES",
     "CONTEXT_TYPES",
+    "MATERIALIZER_POLICY_ID",
+    "MATERIALIZER_SELECTION_RULE",
     "MarketContextBuildError",
     "MarketContextContractError",
     "MarketContextFrame",
+    "MarketContextMaterializationError",
     "MarketContextStore",
     "build_market_context",
+    "materialize_market_context",
     "market_context_status",
+    "select_durable_representation_frames",
+    "validate_market_context_materializations",
     "validate_market_context_store",
+    "verify_materialized_context",
 ]
