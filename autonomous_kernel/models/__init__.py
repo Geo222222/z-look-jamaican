607764b71c9b4765725f6c11936016b348a32ed4
B Geo 22 <161180938+Geo222222@users.noreply.github.com>
2026-09-04 10:33:06 -0500

    Expose governed model qualification bridge

--- autonomous_kernel/models/__init__.py
+++ autonomous_kernel/models/__init__.py
@@ -14,6 +14,16 @@ from .baselines import (
     run_baseline_models,
 )
 from .contracts import ModelDefinition, ModelDefinitionError
+from .qualification import (
+    QUALIFICATION_AUTHORITY,
+    ModelQualificationError,
+    QualificationEvidenceStore,
+    apply_transition_proposal,
+    build_evaluation_receipt,
+    build_transition_proposal,
+    validate_evaluation_receipt,
+    validate_transition_proposal,
+)
 from .registry import ModelRegistry, ModelRegistryError, validate_model_registry
 
 __all__ = [
@@ -23,9 +33,17 @@ __all__ = [
     "ModelDefinitionError",
     "ModelRegistry",
     "ModelRegistryError",
+    "ModelQualificationError",
+    "QualificationEvidenceStore",
+    "QUALIFICATION_AUTHORITY",
     "NullPriorModel",
     "ReportedFlowLinearModel",
+    "apply_transition_proposal",
     "baseline_model_set",
+    "build_evaluation_receipt",
+    "build_transition_proposal",
     "run_baseline_models",
+    "validate_evaluation_receipt",
     "validate_model_registry",
+    "validate_transition_proposal",
 ]
