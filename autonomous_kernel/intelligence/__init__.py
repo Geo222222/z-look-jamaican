690aec88bcde1d9f954184580e616df44324e12f
B Geo 22 <161180938+Geo222222@users.noreply.github.com>
2026-09-04 09:52:10 -0500

    Create ZLJ intelligence publication package

---
+++ autonomous_kernel/intelligence/__init__.py
@@ -0,0 +1,13 @@
+from .publication import (
+    INTELLIGENCE_AUTHORITY,
+    IntelligencePublicationError,
+    build_intelligence_publication,
+    validate_intelligence_publication,
+)
+
+__all__ = [
+    "INTELLIGENCE_AUTHORITY",
+    "IntelligencePublicationError",
+    "build_intelligence_publication",
+    "validate_intelligence_publication",
+]
