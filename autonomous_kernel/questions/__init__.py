ddb967297db6e036e7789bb0b34c977468925aa2
B Geo 22 <161180938+Geo222222@users.noreply.github.com>
2026-09-03 21:19:53 -0500

    feat(questions): export versioned reversal question contract

--- autonomous_kernel/questions/__init__.py
+++ autonomous_kernel/questions/__init__.py
@@ -16,6 +16,14 @@ from .contracts import (
     build_question_registry_snapshot,
 )
 from .evidence import material_question_registry_evidence
+from .evolution import (
+    REVERSAL_QUESTION_V1_1_REF,
+    REVERSAL_QUESTION_V1_REF,
+    REVERSAL_ROOT_PATH_RESOLVER_IMPLEMENTATION_REF,
+    REVERSAL_ROOT_PATH_RESOLVER_POLICY_ID,
+    build_reversal_v1_1_registry,
+    reversal_question_v1_1,
+)
 from .learning_evidence import (
     LearningEvidenceError,
     LearningJournalCommitment,
@@ -24,6 +32,7 @@ from .learning_evidence import (
 from .readiness import (
     RESOLVER_READY_IMPLEMENTATIONS_V1,
     UNRESOLVED_QUESTION_IDS_V1,
+    build_complete_resolver_ready_registry_v1_1,
     build_resolver_ready_registry,
     build_resolver_ready_registry_v1,
     resolver_ready_refs_v1,
@@ -34,6 +43,10 @@ __all__ = [
     "QUESTION_REGISTRY_SCHEMA_VERSION",
     "QUESTION_SCHEMA_VERSION",
     "RESOLVER_READY_IMPLEMENTATIONS_V1",
+    "REVERSAL_QUESTION_V1_1_REF",
+    "REVERSAL_QUESTION_V1_REF",
+    "REVERSAL_ROOT_PATH_RESOLVER_IMPLEMENTATION_REF",
+    "REVERSAL_ROOT_PATH_RESOLVER_POLICY_ID",
     "UNRESOLVED_QUESTION_IDS_V1",
     "AnswerKind",
     "LearningEvidenceError",
@@ -45,12 +58,15 @@ __all__ = [
     "QuestionRegistryEntry",
     "QuestionRegistrySnapshot",
     "QuestionScope",
+    "build_complete_resolver_ready_registry_v1_1",
     "build_learning_journal_commitment",
     "build_question_registry_snapshot",
     "build_resolver_ready_registry",
     "build_resolver_ready_registry_v1",
+    "build_reversal_v1_1_registry",
     "default_question_registry_v1",
     "material_question_registry_evidence",
     "question_catalog_v1",
     "resolver_ready_refs_v1",
+    "reversal_question_v1_1",
 ]
